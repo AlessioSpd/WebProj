f20793f058d23bd7ec39bc6fdfc4dd00116c6874
AlessioSpd <alessiospadafora.main@gmail.com>
2023-02-22 20:09:19 +0100

    reload

---
+++ src/main/java/com/Tidy/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.Tidy.request;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class LoginRequest {
+    String email;
+    String password;
+}
